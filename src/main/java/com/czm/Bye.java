fd3361ee8cd5d41a58a916a1c3eeb95f521f8695
czm <zimingmorning520@gmail.com>
2024-04-28 17:48:47 -0400

    bye.class added

---
+++ src/main/java/com/czm/Bye.java
@@ -0,0 +1,12 @@
+package com.czm;
+
+/**
+ * @author Ziming Chen
+ * @version 1.0
+ */
+public class Bye {
+    public static void main(String[] args) {
+
+        System.out.println("byebye...");
+    }
+}
